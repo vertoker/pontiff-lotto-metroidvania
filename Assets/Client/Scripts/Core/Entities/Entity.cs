9f5ac7deb0303025e3197e83b2f5c4b4d26e4735
vertoker <vertogpro@gmail.com>
2022-02-23 12:20:12 +0500

    add entity

---
+++ Assets/Client/Scripts/Core/Entities/Entity.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Core.Entities
+{
+    public class Entity : MonoBehaviour
+    {
+        [SerializeField] private Stats _baseStats;
+
+        // Сет брони
+
+        // Пассивные эффекты
+
+        // Активные способности
+    }
+}
